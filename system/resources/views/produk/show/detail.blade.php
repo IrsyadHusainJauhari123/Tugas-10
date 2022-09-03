df647c91891df5f63e334dd8d0c579cbe69ff0cd
IrsyadHusainJauhari123 <108142445+IrsyadHusainJauhari123@users.noreply.github.com>
2022-09-04 00:14:29 +0700

    Initial commit

---
+++ system/resources/views/produk/show/detail.blade.php
@@ -0,0 +1,5 @@
+{{ $produk->harga }} |
+Stok : {{ $produk->stok }} |
+Berat : {{ $produk->berat }} kg |
+Seller : {{ $produk->seller->nama }} |
+Tanggal Produk : {{ $produk->created_at->diffForHumans() }}
