fc87eb551846f414d3341f2d98e6737504660db1
IrsyadHusainJauhari123 <108142445+IrsyadHusainJauhari123@users.noreply.github.com>
2022-09-07 20:45:44 +0700

    Update detail.blade.php

--- system/resources/views/produk/show/detail.blade.php
+++ system/resources/views/produk/show/detail.blade.php
@@ -1,5 +1,7 @@
-{{ $produk->harga }} |
-Stok : {{ $produk->stok }} |
-Berat : {{ $produk->berat }} kg |
-Seller : {{ $produk->seller->nama }} |
-Tanggal Produk : {{ $produk->created_at->diffForHumans() }}
+  <p>
+      {{ $produk->harga }} |
+      Stok : {{ $produk->stok }} |
+      Berat : {{ $produk->berat }} kg |
+      Seller : {{ $produk->seller->nama }} |
+      Tanggal Produk : {{ $produk->created_at->diffForHumans() }}
+  </p>
